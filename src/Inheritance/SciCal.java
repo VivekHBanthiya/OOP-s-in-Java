bee946c4efcd859305518deeae07c6285d55e713
VivekHBanthiya <vivekbanthiya31@gmail.com>
2024-03-08 16:20:10 +0530

    Second

---
+++ src/Inheritance/SciCal.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class SciCal extends AdvCal{
+    int power(int a, int b){
+        return (int)Math.pow(a,b);
+    }
+    double log(int a){
+        return Math.log(a);
+    }
+}
