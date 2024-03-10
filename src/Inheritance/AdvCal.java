42b864fe2f67da18621aef3cad91704acc10778d
VivekHBanthiya <vivekbanthiya31@gmail.com>
2024-03-10 23:55:06 +0530

    refactor

--- src/Inheritance/AdvCal.java
+++
@@ -1,6 +0,0 @@
-package Inheritance;
-
-public class AdvCal extends SimpCal{
-    int mul(int a, int b){return a*b;}
-    int div(int a, int b){return a/b;}
-}
--- src/Inheritance/SimpCal.java
+++
@@ -1,7 +0,0 @@
-package Inheritance;
-
-public class SimpCal {
-    int add(int a, int b){return a+b;}
-    int sub(int a, int b){return a-b;}
-
-}
